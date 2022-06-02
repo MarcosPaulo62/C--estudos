7cdefaa7af443cd5552d616405b89532a7eaf3d4
MarcosPaulo62 <markossylva62@gmail.com>
2022-06-02 15:27:54 -0300

    Adicionando POO/pooMaiorIdade

---
+++ POO/pooMaiorIdade/pooMaiorIdade/Entities/Pessoa.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pooMaiorIdade.Entities
+{
+    public class Pessoa
+    {
+        public Pessoa()
+        {
+        }
+
+        public Pessoa(string nome, int idade)
+        {
+            this.Nome = nome;
+            this.Idade = idade;
+        }
+
+        private string nome;
+        public string Nome
+        {
+            get { return nome; }
+            set { nome = value.ToUpper(); }
+        }
+
+        private int idade;
+        public int Idade {
+            get { return idade; }
+            set {
+                if (idade >= 0)
+                    idade = value;
+                else
+                    idade = 0;
+            }
+        }
+
+        public void ExibirDados()
+        {
+            Console.WriteLine($"Nome: {Nome}\nIdade: {Idade}");
+        }
+
+    }
+}
---
+++ POO/pooMaiorIdade/pooMaiorIdade/Program.cs
@@ -0,0 +1,35 @@
+﻿using pooMaiorIdade.Entities;
+
+Pessoa[] pessoas = new Pessoa[3];
+
+for (int i = 0; i < pessoas.Length; i++)
+{
+    Console.WriteLine($"Nome da {i + 1}ª pessoa:");
+    string nome = Console.ReadLine();
+
+    Console.WriteLine($"Idade da {i + 1}ª pessoa:");
+    int idade = int.Parse(Console.ReadLine());
+
+    Pessoa pessoa = new(nome, idade);
+    pessoas[i] = pessoa;
+}
+
+if (pessoas[0].Idade == pessoas[1].Idade && pessoas[1].Idade == pessoas[2].Idade)
+    Console.WriteLine("Todas as pessoas têm a mesma idade.");
+else
+{
+    int maiorIdade = 0;
+    Pessoa maisIdade = new();
+    foreach (Pessoa pessoa in pessoas)
+    {
+        if(pessoa.Idade > maiorIdade)
+        {
+            maiorIdade = pessoa.Idade;
+            maisIdade.Nome = pessoa.Nome;
+            maisIdade.Idade = pessoa.Idade;
+        }
+    }
+
+    Console.WriteLine("\nA pessoa mais velha é:");
+    maisIdade.ExibirDados();
+}
